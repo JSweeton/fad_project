cb58b071865e1b5a15476cc38255501e9e748a2a
Corey Bean <searock35@gmail.com>
2020-10-31 21:50:16 -0400

    Added serial reading for esp32 from USB UART

--- esp_tests/main/main.c
+++ esp_tests/main/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/timers.h"
@@ -11,7 +12,18 @@
 #include "driver/uart.h"
 
 const char TEST_TAG[] = "FAD_TEST";
+QueueHandle_t uart_handle;
 
+
+int write_to_uart(char *data) {
+    int len = strlen(data);
+    int written_bytes = 0;
+    if(len > 0) {
+       written_bytes = uart_write_bytes(UART_NUM_0, data, len);
+    }
+    
+    return written_bytes;
+}
 void app_main(void) {
 
     const int uart_num = UART_NUM_0;
@@ -20,22 +32,51 @@ void app_main(void) {
         .data_bits = UART_DATA_8_BITS,
         .parity = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_CTS_RTS,
-        .rx_flow_ctrl_thresh = 122,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
     };
     // Configure UART parameters
     ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
-    // ESP_LOGI(TEST_TAG, "Configuring UART parameters");
+    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_0, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+    ESP_ERROR_CHECK(uart_driver_install(uart_num, 256, 256, 10, &uart_handle, 0));
+    ESP_LOGI(TEST_TAG, "Beginning initialization");
+    char *in_buffer = (char *)malloc(50 * sizeof(char));
+    char *out_buffer = (char *)malloc(50 * sizeof(char));
+    assert(in_buffer);
+    // ESP_LOGI(TEST_TAG, "Successfully initialized");
+    assert(out_buffer);
+    ESP_LOGI(TEST_TAG, "Successfully initialized");
 
-    char *buffer = (char *)malloc(50 * sizeof(char));
-    int written_bytes = 0;
+    //give some cool down time
+    vTaskDelay(100);
+    int read_bytes = uart_read_bytes(uart_num, in_buffer, 45, 100);
+    
+    char output[50];
 
-    while(1) {
-        vTaskDelay(10);
-        int read_bytes = uart_read_bytes(uart_num, buffer, 1, 10);
+    if(strlen(in_buffer) > 0) {
+        sprintf(output, "0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x", 
+                        in_buffer[0], in_buffer[1], in_buffer[2], in_buffer[3], in_buffer[4], in_buffer[5], in_buffer[6], in_buffer[7]);
+        ESP_LOGI(TEST_TAG, "Read msg, num: %s", output);
+    } 
 
-        if (read_bytes != -1) {
-            written_bytes = uart_write_bytes(uart_num, buffer, 1);
-        }
-    }
+    ESP_LOGI(TEST_TAG, "Attempted read");
+    out_buffer[0] = 'y';
+    char msg[50];   
+    // sprintf(msg, "This is my test. Please hear my prayers.\n");
+    // int written_bytes = write_to_uart(msg);
+    // ESP_LOGI(TEST_TAG, "Wrote %d bytes", written_bytes);
+
+
+
+    // int written_bytes = 0;
+    // while(1) {
+    //     vTaskDelay(10);
+    //     int read_bytes = uart_read_bytes(uart_num, in_buffer, 1, 10);
+
+    //     if (read_bytes != -1) {
+    //         // out_buffer[0] = in_buffer[0];
+    //         ESP_LOGI(TEST_TAG, "Read a byte!");
+    //         // written_bytes = uart_write_bytes(uart_num, out_buffer, 1);
+    //     }
+    // }
 }
--- uart_tester/esp_com.py
+++ uart_tester/esp_com.py
@@ -1,32 +1,61 @@
 import serial
 import keyboard
+import typing
+from sys import platform
 
+key_input = 0
 
-def set_key_input(char):
+def set_key_input(KeyEvent: keyboard.KeyboardEvent):
     global key_input
 
-    key_input = char
+    key_input = KeyEvent.name 
 
+    # debug
+    # print(key_input)
 
-def print_input(com_instance):
+def print_input(com_instance: serial.Serial):
 
-    global key_input
+    print("Successfully opened com")
 
     while True:
+        global key_input
         input_bytes = com_instance.read()
         print(input_bytes.decode(), end='')
-        output_bytes = input_bytes
-        
-def main():
+        # for now, continually output a 'y'
+        output_bytes = bytes('yyyyy\n', 'utf-8')
+        com_instance.write(output_bytes)
+       # if key_input != 0:
+        #     output_string = key_input
+        #     key_input = 0
+        #     output_bytes = bytes(output_string, 'utf-8')  
+        #     com_instance.write(output_bytes) 
 
-    keyboard.on_press_key("p", lambda _: set_key_input('p'))
+def main():
+    if platform == "linux" or platform == "linux2":
+        com_port = "/dev/ttyUSB0"
+        print(f'Running linux system, com port is {com_port}')
+    elif platform == "darwin":
+        com_port = '' #not sure
+        print(f'Running MAC, com port is {com_port}')
+    elif platform == "win32":
+        com_port = "COM5"
+        print(f'Running windows system, com port is {com_port}')
+
+    # keyboard.on_press_key("q", lambda _: set_key_input('q'))
+    # keyboard.on_press_key("w", lambda _: set_key_input('w'))
+    # keyboard.on_press_key("e", lambda _: set_key_input('e'))
+    # keyboard.on_press_key("r", lambda _: set_key_input('r'))
+
+    keyboard.hook(set_key_input)
 
     try:
-        esp_com = serial.Serial("COM5", baudrate=115200)
+        esp_com = serial.Serial(com_port, baudrate=115200)
         print_input(esp_com)
     except serial.SerialException:
         print("COM port not opened")
 
+    while(True):
+       sudo = True
 
 main()
 
