501f88adf92512a1b3f9400783e62e61a291025e
SageOmegaVega <ljrv15@gmail.com>
2021-03-30 11:22:58 -0400

    Began Algo-shift

--- fad_algorithms/algo_freq_shift.c
+++ fad_algorithms/algo_freq_shift.c
@@ -1,43 +1,68 @@
 /**
- * algo_freq_shift.c
- * Author: Corey Bean,
+ * algo_template.c
+ * Author: Larry Vega,
  * Organization: Messiah Collaboratory
- * Date: 1/26/2021
+ * Date: 3/25/2021
  *
  * Description:
- * This algorithm takes the input signal and shifts it in the frequency domain by some target frequency.
+ * This file returns the input signal at a different pitch, shifted by some target amount.
  */
 
+
+
 #include "algo_freq_shift.h"
+#include "fad_defs.h"
+#include <stdlib.h>
+#include <math.h>
 
-void algo_freq_shift(uint16_t *in_buff, uint8_t *out_buff, uint16_t in_pos, uint16_t out_pos, int multisamples) {
 
-    /**
-     * in_pos and out_pos point to the algorithm half of the buffer. Only ready and write to data in the half
-     * of the buffer designated by these positions
-     * 
-     * The algorithm should have minimum side effects: try not to write to globals defined in other files, etc.
-     */
+static double algo_freq_read_size_g;
 
-    /* This algorithm simply outputs the input to the ADC */
-    for (int i = 0; i < algo_freq_shift_read_size / multisamples; i++) {
+static double pitch_freq;
 
-        //the following section averages the multisampled ADC data so that each DAC buffer space will have one corresponding ADC value
-        uint16_t avg = 0;
+static double[] shift_array = {};
+static double[] input_data = {};
 
-        for (int j = 0; i < multisamples; j++) {
-            avg += in_buff[in_pos + (i * multisamples) + j];
-        }
 
-        avg = avg / multisamples;
-        /** END SECTION **/
+
+void algo_freq_shift(uint16_t *in_buff, uint8_t *out_buff, uint16_t in_pos, uint16_t out_pos, int multisamples){
+
+    double[] input_data = {};
+    double[] output_data = {};
+
+    for (int i = 0; i < ADC_BUFFER_SIZE; i++){
+
+        input_data[i] = in_buff[in_pos + i];
+        output_data[i] = input_data * shift_array[i];
+
+        out_buff[i] = output_data[out_pos + i];
+
         
-        out_buff[out_pos + i] = avg >> 4;
+
+
+        
+
     }
 
+    
+
+
+    
 }
 
-void algo_freq_shift_init(int algo_size, int shift_size) {
-    algo_freq_shift_read_size = algo_size;
-    shift_size_g = shift_size;
+
+
+
+
+
+
+void algo_freq_init(int algo_size, int pitch_freq){
+    algo_freq_read_size_g = algo_size;
+
+
+
+    for int(i = 0, i < ADC_BUFFER_SIZE, i++){
+        shift_array[i] = sin(i * (pitch_freq) / OUPUT_FREQ);
+    }
+
 }
--- fad_algorithms/include/algo_freq_shift.h
+++ fad_algorithms/include/algo_freq_shift.h
@@ -5,31 +5,32 @@
  * Date: 10/12/2020
  *
  * Description:
- * This algorithm takes the input signal and shifts it in the frequency domain by some target frequency.
+ * This file returns the input signal at a different pitch, shifted by some target amount.
  */
+
+
 
 #include <stdint.h>
+#include <math.h>
 
-/* Defines how many values the algorithm will read from the ADC buffer. Should always be at least half of buffer size. */
-int algo_freq_shift_read_size;
 
-/* Define any globals */
 
-/* The frequency shift factor, which will determine the frequency shift amount. */
-int shift_size_g;
 
-/**
- * @brief Frequency shift algorithm for ESP masker
- * @param in_buff Buffer that points to the beginning of the ADC data
- * @param out_buff [OUT] Buffer that points to the beggining of DAC data staged to be output to the DAC
- * @param in_pos Points to starting point of this algorithm chunk
- * @param out_pos Points to starting point of this algorithm chunk
- * @param multisamples Number of input samples per output sample
- */
-void algo_freq_shift(uint16_t *in_buff, uint8_t *out_buff, uint16_t in_pos, uint16_t out_pos, int multisamples); 
 
-/**
- * @brief Initializes algorithm constants
- * @param algo_size The amount of data to process from the input
- */
-void algo_freq_shift_init(int algo_size, int shift_size);
+//double algo_freq_read_size_g;
+
+// double pitch_freq;
+
+// double[] shift_array = {};
+// double[] input_data = {};
+
+
+
+
+
+
+
+
+
+
+
